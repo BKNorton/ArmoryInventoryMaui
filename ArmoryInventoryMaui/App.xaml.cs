8f3018bab2d3bb2534f384fef85d8c3de1499644
Kirk Norton <bknorton01@gmail.com>
2024-09-09 02:47:18 -0400

    initial commit

---
+++ ArmoryInventoryMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ArmoryInventoryMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ArmoryInventoryMaui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ArmoryInventoryMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
